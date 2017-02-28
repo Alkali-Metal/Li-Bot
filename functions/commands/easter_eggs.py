e6343f6726d97d6124fb028e2606ba114ab64553
Alkali-Metal <alkali.maps@gmail.com>
2017-02-27 18:38:14 -0700

    Added partial command type parsing for more types of commands then plaintext.

--- functions/commands/easter_eggs.py
+++ functions/commands/easter_eggs.py
@@ -1,1 +1,8 @@
-print("EEC")
+import random
+
+def random_dance():
+    dance_moves = ['<(^-^<)','(>^-^)>','<(^-^<)','(>^-^)>','<(^-^<)','(>^-^)>','┻━┻︵ ¯\_(ツ)_/¯︵ ┻━┻']
+    randomized_dance = random.shuffle(dance_moves)
+    randomized_dance = """
+""".join(dance_moves)
+    return randomized_dance
--- main.py
+++ main.py
@@ -16,26 +16,28 @@ async def on_ready():
     print('Logged in as: {user}'.format(user=client.user.name))
 
 
-#def update():
-#    if message.content.startswith(">"):
-#        if message.author.id == owner_id:
-#            await client.send_message(message.channel, "Updating the bot. :smile:")
-#            importlib.reload(parser)
-#            importlib.reload(functions.configs.general_config)
-#        else:
-#            await client.send_message(message.channel, "You don't have access to that command")
-
-
-
 @client.event
 async def on_message(message):
-    if message.content.startswith(command_prefix):
-        roles = []
-        for role in message.author.roles:
-            roles.append(role.name)
+    if message.content.startswith(">"):
+        if message.author.id == owner_id:
+            await client.send_message(message.channel, "Updating the bot :smile:")
+            #shutil.rmtree("functions")    COMMENTED TO REDUCE RISK CURRENTLY
+            #importlib.reload(functons.parser)
+            #importlib.reload(functions.configs.general_config)
+        else:
+            await client.send_message(message.channel, "You don't have access to that command!")
+    elif message.content.startswith(command_prefix):
         command, *args = message.content[1:].split()
-        parser.info(a_id=message.author.id, a_name=message.author.name, s_id=message.server.id, a_nickname=message.author.display_name, s_name=message.server.id, a_roles=message.author.roles)
         print(command, args)
+        parser.info(a_id=message.author.id, a_name=message.author.name, s_id=message.server.id, a_nickname=message.author.display_name, s_name=message.server.id, a_roles=message.author.roles)
+        #command_type = parser.command_type(command)
+        #if command_type == "text":
         await client.send_message(message.channel, parser.main(command, args, message=message))
+        #elif command_type == "role_assignment":
+        #    await client.add_roles(message.channel, parser.main(command, args, message=message))
+        #elif command_type == "file_upload":
+        #    await client.upload_file(message.channel. parser.main(command, args, message=message))
+#        elif command_type == "":
+
 
 client.run(bot_token)
