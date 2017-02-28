47978c72d8757376d51b817e0b08e46982cb2c28
Alkali-Metal <alkali.maps@gmail.com>
2017-02-27 18:46:38 -0700

    Added base code for the moderator and admin handlers

--- functions/handler.py
+++ functions/handler.py
@@ -12,8 +12,20 @@ import functions.commands.easter_eggs as EEC
 import functions.commands.fun as fun
 import functions.commands.test as test
 
-def user(command, *args, server_id):
+
+def type(command):
+    type_of_command = command.info()
+    return type_of_command
+
+def user(command, *args, server_id, author_id):
     if command == "test":
         return test.run()
     if command == "help":
         return help.run(server_id=server_id)
+    if command == "randomdance":
+        return EEC.random_dance()
+
+#def moderator(command, *args):
+
+
+#def admin(command, *args):
