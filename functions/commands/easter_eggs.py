16c0a98535f8efe07511bc53ebe235f5a98a8c55
Alkali-Metal <alkali.maps@gmail.com>
2017-03-03 01:48:19 -0700

    Deprecated. So I deleted

--- functions/commands/easter_eggs.py
+++
@@ -1,8 +0,0 @@
-import random
-
-def random_dance():
-    dance_moves = ['<(^-^<)','(>^-^)>','<(^-^<)','(>^-^)>','<(^-^<)','(>^-^)>','┻━┻︵ ¯\_(ツ)_/¯︵ ┻━┻']
-    randomized_dance = random.shuffle(dance_moves)
-    randomized_dance = """
-""".join(dance_moves)
-    return randomized_dance
