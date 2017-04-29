e797fb31657939362c889ea3d9475a991854bd09
Alkali-Metal <alkali.maps@gmail.com>
2017-04-29 17:53:14 -0600

    Fixed the errors?

--- functions/processing/message.py
+++ functions/processing/message.py
@@ -1,44 +1,77 @@
-import discord
-import functions.configs.general_config as config
-import functions.processing.permissions as permissions
-import functions.processing.server_commands as servers
+#-----------------------------------------------------------------------------#
 
 
 
-#class parser:
-# Yes I did have the indentation correct for the class, I just undid it before commiting
-global lock
-lock = "false"
 
 
 
-def main(client, message):
-    if message.content.startswith(config.command_prefix):
-        command, *args = message.content[1:].split()
-        if lock == "false":
-            print(message.server.name)
-            server_name = message.server.name.replace(" ","_")
-            print(server_name)
-            if message.server.name in servers.server_list:
-                if command in servers.server_list[server_name]:
-                    permissions = permissions.checking
-                    if permissions == "allowed":
-                        return servers.server_list[server_name[command]
-                            (client, message, *args)]
-                    elif permissions == "blocked":
-                        return client.send_message(message.channel,
-                            "Invalid Permissions")
-        elif lock == "true":
-            if message.server.id != "219893635439656961":
-                return "null"
-            elif message.server.id == "219893635439656961":
-                return servers.server_list[Bot_Test_Server[command]
-                    (client, message, *args)]
-    elif message.content.startswith(">"):
-        command, *args = message.content[1:].split()
-        if command == "lock":
-            if lock == "false":
-                lock = "true"
-            elif lock == "true":
-                lock = "false"
-            print("The lock has changed to {}".format(lock))
+
+
+#-----------------------------------------------------------------------------#
+
+
+import functions.processing.permissions as permission
+import functions.configs.user_configs.general_config as config
+import functions.processing.config_checks as config_checking
+from functions.configs.developer_configs import command_config
+
+
+
+def main(client, message, lock, *args):
+    if message.channel.is_private == False:
+        print("Message in Server")
+        if message.server.id in config.server_command_prefix:
+            if message.content.startswith(config.server_command_prefix[
+                message.server.id]):
+                return handling(lock, client, message, *args)
+        elif message.content.startswith(config.command_prefix):
+            return handling(lock, client, message, *args)
+    else:
+        print("Direct Message")
+        if message.content.startswith(config.command_prefix):
+            handling(lock, client, message, *args)
+
+def handling(lock, client, message, *args):
+    print("Handler")
+    command, *args = message.content[1:].split()
+#    print(command, args)
+    if message.channel.is_private == False:
+        print("Public Channel - Public Checking")
+        permission_level = permission.public_checking(lock, client, message,
+            command, *args)
+    elif message.channel.is_private == True:
+        print("Direct Channel - Private Checking")
+        permission_level = permission.private_checking(client, message,
+            command, *args)
+    else:
+        print("Channel Type Not Supported Yet")
+#   Executing the command if permissions allow
+    if permission_level == "allowed":
+        print("Has returned \"allowed\"")
+        if command in command_config.non_biased:
+            return command_config.non_biased[command](client, message, command,
+                *args)
+        elif command in command_config.server_only:
+            return command_config.server_only[command](client, message, command,
+                *args)
+        elif command in command_config.direct_only:
+            return command_config.direct_only[command](client, message, command,
+                *args)
+    elif permission_level == "disallowed":
+        print("Has returned \"disallowed\"")
+        return client.send_message(message.channel, "Invalid permissions")
+    elif permission_level == "invalid command":
+        print("Has returned \"Invalid command\"")
+        return client.send_message(message.channel, "Invalid command")
+    elif permission_level == "server only":
+        print("Has returned \"server only\"")
+        return client.send_message(message.channel, "That command is server only")
+    elif permission_level == "restricted":
+        print("Has returned \"resticted\"")
+        return client.send_message(message.channel, "That command is restricted")
+    elif permission_level == "null":
+        print("Has returned \"null\"")
+        return None
+    else:
+        print("PANIC AND RUN!")
+        print("Invalid permission response")
